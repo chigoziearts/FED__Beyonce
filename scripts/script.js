9e632dc87e8fac01a852a40c638236d94a8f0aa5
chigoziearts <70476735+chigoziearts@users.noreply.github.com>
2021-09-16 15:45:40 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+console.log("ch3ck");
+
+var hamburgerknop = document.querySelector("#Hamburger");
+
+var navigatie = document.querySelector("div.menudicht");
+
+hamburgerknop.addEventListener('click', menuopen);
+
+function menuopen(){
+    navigatie.classList.toggle("menu");
+}
+
